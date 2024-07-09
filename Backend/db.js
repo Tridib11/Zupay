c712fb1e5e9b046aaffffe117be4d3a05af2fe12
Tridib11 <tridibghosh12345@gmail.com>
2024-07-10 01:41:35 +0530

    chore: Add account model to db.js

--- Backend/db.js
+++ Backend/db.js
@@ -11,8 +11,22 @@ const userSchema = mongoose.Schema({
   lastName: String,
 });
 
+const accountSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: number,
+    required: true,
+  },
+});
+
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
   User,
+  Account,
 };
