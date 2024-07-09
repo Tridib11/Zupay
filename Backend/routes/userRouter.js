6cd84135754c8482947365d1ed5245c73bbc3e23
Tridib11 <tridibghosh12345@gmail.com>
2024-07-10 00:12:49 +0530

    chore: Update zod package to version 3.23.8

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,3 +1,12 @@
 const express=require("express")
+const zod=require("zod")
 const router=express.Router()
+
+const signupBody=zod.object({
+  username:zod.string().email(),
+  firstName:zod.string(),
+  lastName:zod.string(),
+  password:zod.string().min(6)
+})
+
 module.exports=router
