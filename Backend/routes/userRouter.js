52be395a87f79d1ef1614d733b8d9375b9989b72
Tridib11 <tridibghosh12345@gmail.com>
2024-07-10 11:04:51 +0530

    chore: Add account creation during user signup

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const zod = require("zod");
-const { User } = require("../db");
+const { User, Account } = require("../db");
 const { JWT_SECRET } = require("../config");
 const authMiddleware = require("../middleware");
 const router = express.Router();
@@ -38,6 +38,11 @@ router.post("/signup", async (req, res) => {
   });
 
   const userId = user._id;
+
+  await Account.create({
+    userId,
+    balance:1+Math.random()*10000
+  })
 
   const token = jwt.sign(
     {
