c073dce702301ab010e491e0721eb00e85e405b9
Tridib11 <tridibghosh12345@gmail.com>
2024-07-10 11:07:12 +0530

    chore: Update userRouter.js to include balance creation during user signup

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -41,8 +41,8 @@ router.post("/signup", async (req, res) => {
 
   await Account.create({
     userId,
-    balance:1+Math.random()*10000
-  })
+    balance: 1 + Math.random() * 10000,
+  });
 
   const token = jwt.sign(
     {
