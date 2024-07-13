9b35a398d33991a0aede201c495a485623edbd38
Tridib11 <tridibghosh12345@gmail.com>
2024-07-14 00:03:31 +0530

    FIle added

--- Frontend/src/pages/Signin.jsx
+++ Frontend/src/pages/Signin.jsx
@@ -1,6 +1,14 @@
- function Signin(){
-  return <div>
-    signin
+import Heading from "../components/Heading.jsx";
+import SubHeading from "../components/SubHeading.jsx";
+
+function Signin(){
+  return <div className="bg-slate-300 h-screen flex justify-center">
+    <div className="flex flex-col justify-center">
+      <div className="rounded-lg bg-white w-80 text-center p-2 px-4">
+        <Heading label={"Sign in"}/>
+        <SubHeading label={"Enter your credentials to access your account"}/>
+      </div>
+    </div>
   </div>
 }
 export default Signin
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,6 +1,25 @@
- function Signup(){
-  return <div>
-    signup
+import Heading from "../components/Heading.jsx";
+import SubHeading from "../components/SubHeading.jsx";
+import {InputBox} from "../components/InputBox.jsx";
+import {Button} from "../components/Button.jsx";
+
+function Signup(){
+  return <div className="bg-slate-300 h-screen flex justify-center">
+    <div className="flex flex-col justify-center">
+      <div className="rounded-lg bg-white w-80 text-center p-2  px-4">
+        <Heading label={"Sign up"}/>
+        <SubHeading label={"Enter your information to create an account"}/>
+        <InputBox placeholder={"John"} label={"First name"}/>
+        <InputBox placeholder={"Doe"} label={"Last name"}/>
+        <InputBox placeholder={"johndoe@gmail.com"} label={"Email"}/>
+        <InputBox placeholder={""} label={"Password"}/>
+        <div className="pt-4">
+          <Button label="Sign up"/>
+        </div>
+      </div>
+    </div>
   </div>
+
 }
+
 export default Signup
